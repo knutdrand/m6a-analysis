2d2b77e719ed23f07f3a2c11acc0791f10d57a41
Knut Rand <knutdrand@gmail.com>
2020-09-28 13:47:07 +0200

    yrange in metaenrichment

--- scripts/enrichment_plot.py
+++ scripts/enrichment_plot.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 ip, control = [np.load(f) for f in snakemake.input]
 enrichment = (ip/np.sum(ip))/(control/np.sum(control))
 plt.plot(enrichment)
-plt.vlines([124, 1124], ymin=0, ymax=np.max(enrichment))
+plt.vlines([124, 1124], ymin=0.5, ymax=1.5)
 plt.hlines([1], xmin=0, xmax=enrichment.size)
 plt.savefig(snakemake.output[0])
